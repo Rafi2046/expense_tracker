cafdfdcbe2243c543dfc864e1fdde632926e2c73
Rafi2046 <onestappdev103@gmail.com>
2026-07-09 23:42:20 +0600

    bug fixes

---
+++ lib/core/constants/app_font_sizes.dart
@@ -0,0 +1,25 @@
+class AppFontSizes {
+  AppFontSizes._();
+
+  // PDF-specific small sizes
+  static const double size6 = 6;
+  static const double size7 = 7;
+  static const double size8 = 8;
+
+  static const double size9 = 9;
+  static const double size10 = 10;
+  static const double size11 = 11;
+  static const double size12 = 12;
+  static const double size13 = 13;
+  static const double size14 = 14;
+  static const double size15 = 15;
+  static const double size16 = 16;
+  static const double size18 = 18;
+  static const double size20 = 20;
+  static const double size22 = 22;
+  static const double size24 = 24;
+  static const double size28 = 28;
+  static const double size32 = 32;
+  static const double size36 = 36;
+  static const double size40 = 40;
+}
